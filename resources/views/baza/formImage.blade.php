84feb039860974ebd2c1bb33afc3e708f415b9ad
alek101 <47416711+alek101@users.noreply.github.com>
2019-11-10 17:27:39 +0100

    Verzija 1.9.3.1
    save

--- resources/views/baza/formImage.blade.php
+++ resources/views/baza/formImage.blade.php
@@ -9,7 +9,6 @@
         
         <div class="flexRow">
             <label for="slika">Slika: <input type="file" name="slika" id="slika"></label>
-            
             <label for="naziv">Naziv slike sa ekstenzijom:<input type="text" name="nazivSlike" id="naziv"></label>
         </div>
         <div><input type="submit" value="Posalji" id='dugme'></div> 
@@ -20,8 +19,7 @@
 <script>
     document.querySelector('#slika').addEventListener('change',function(e)
     {
-        let r=document.querySelector('#slika').value.slice(12);
-        document.querySelector('#naziv').value=r;
+        document.querySelector('#naziv').value=document.querySelector('#slika').value.slice(12);
     })
 
 </script>
