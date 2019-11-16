1a63f3098a978618034dda5afc234beac85313f9
alek101 <47416711+alek101@users.noreply.github.com>
2019-11-16 14:35:23 +0100

    Veryija 2.2.5
    U dve strane gde postoji izbor izmedju broja tablica i broja sasije, forma se menja.

--- resources/views/baza/carModelID.blade.php
+++ resources/views/baza/carModelID.blade.php
@@ -7,10 +7,18 @@
                 @csrf 
         </div>
         <div class="flexRow">
-            <label for="Broj_sasije">Broj Šasije<input type="text" name="Broj_sasije" id="Broj_sasije"></label>
-            <label for="Broj_registarskih_tablica">Broj Tablica<input type="text" name="Broj_registarskih_tablica" id="Broj_registarskih_tablica"></label>
+            <label for="selector">
+                <select name="selector" id="selector">
+                    <option value="tablice" selected>Po tablicama</option>
+                    <option value="sasija">Po broju šasije</option>
+                </select>
+            </label>
         </div>
-        <div><input type="submit" value="Posalji" id='dugme'></div> 
+        <div class="flexRow">
+            <label for="Broj_sasije" class="labSas disapear">Broj Šasije<input type="text" name="Broj_sasije" id="Broj_sasije"></label>
+            <label for="Broj_registarskih_tablica" class="labTab">Broj Tablica<input type="text" name="Broj_registarskih_tablica" id="Broj_registarskih_tablica"></label>
+        </div>
+        <div><input type="submit" value="Pošalji" id='dugme'></div> 
 </form>
 
 <script>
@@ -20,6 +28,12 @@
     {
         document.querySelector('#Broj_registarskih_tablica').value=storedPlates;
     }
+    //izmena forme
+    document.querySelector('#selector').addEventListener('change',function()
+    {
+        document.querySelector('.labSas').classList.toggle('disapear');
+        document.querySelector('.labTab').classList.toggle('disapear');
+    })
 </script>
 
 @endsection
--- resources/views/prijem/prijem.blade.php
+++ resources/views/prijem/prijem.blade.php
@@ -6,15 +6,21 @@
     {{-- <input type="hidden" name="_token" value="{{ csrf_token() }}" id='token'> --}}
     {{-- Broj Sasije  --}}
     <div class="flexRow">
-        <label for="id"></label><input type="text" name='id' value="" id="id" hidden></label>
+        <label for="selector">
+            <select name="selector" id="selector">
+                <option value="tablice" selected>Po tablicama</option>
+                <option value="sasija">Po broju šasije</option>
+            </select>
+        </label>
     </div>
     <div class="flexRow">
-        <label for="t">Broj Tablica <input type="text" name="tablica" id="t" value=""></label>
+        <label for="id" class="labSas disapear">Broj Šasije<input type="text" name='id' value="" id="id"></label>
+        <label for="t" class="labTab">Broj Tablica <input type="text" name="tablica" id="t" value=""></label>
         <label for="km">Dodaj KM <input type="number" name="km" id="km" value="" required></label>
     </div>
     
     <div class="flexRow">
-        <input type="submit" value="Posalji" id="dugme">
+        <input type="submit" value="Pošalji" id="dugme">
     </div>
     
 
@@ -22,4 +28,19 @@
 
 <a href="https://www.animatedimages.org/cat-cars-67.htm"><img src="https://www.animatedimages.org/data/media/67/animated-car-image-0330.gif" border="0" alt="animated-car-image-0330" /></a>
 
+
+<script>
+    //ako postoje tablice u sess stor, upisi ih u formu
+    let storedPlates=sessionStorage.getItem('filterTabela');
+    if(storedPlates!=null)
+    {
+        document.querySelector('#t').value=storedPlates;
+    }
+    //izmena forme
+    document.querySelector('#selector').addEventListener('change',function()
+    {
+        document.querySelector('.labSas').classList.toggle('disapear');
+        document.querySelector('.labTab').classList.toggle('disapear');
+    })
+</script>
 @endsection
