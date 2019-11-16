9462c2952cd189a34c5d69cea846ce47a8572ca6
alek101 <47416711+alek101@users.noreply.github.com>
2019-11-16 14:05:17 +0100

    Verzija 2.2.3
    Ako su tablice u session storegu, upiuje se u pretragu po automobili u admin panelu

--- resources/views/baza/carModelID.blade.php
+++ resources/views/baza/carModelID.blade.php
@@ -7,10 +7,19 @@
                 @csrf 
         </div>
         <div class="flexRow">
-            <label for="Broj_sasije">Broj Sasije<input type="text" name="Broj_sasije" id="Broj_sasije" required></label>
-            <label for="Broj_registarskih_tablica">Broj Tablica<input type="text" name="Broj_registarskih_tablica" id="Broj_registarskih_tablica" required></label>
+            <label for="Broj_sasije">Broj Šasije<input type="text" name="Broj_sasije" id="Broj_sasije"></label>
+            <label for="Broj_registarskih_tablica">Broj Tablica<input type="text" name="Broj_registarskih_tablica" id="Broj_registarskih_tablica"></label>
         </div>
         <div><input type="submit" value="Posalji" id='dugme'></div> 
 </form>
+
+<script>
+    //ako postoje tablice u sess stor, upisi ih u formu
+    let storedPlates=sessionStorage.getItem('filterTabela');
+    if(storedPlates!=null)
+    {
+        document.querySelector('#Broj_registarskih_tablica').value=storedPlates;
+    }
+</script>
 
 @endsection
