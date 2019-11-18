3d7e94545b7cc3780fd58adddb681dd6abb25f09
alek101 <47416711+alek101@users.noreply.github.com>
2019-11-18 15:17:23 +0100

    Verzija 2.7.1
    sitne izmene

--- resources/views/servis/uradi.blade.php
+++ resources/views/servis/uradi.blade.php
@@ -90,7 +90,6 @@
     function upisi(text)
     {
         console.log('opis_upis',opis);
-        // opis.innerHTML+=text;
         opis.value+=text;
     }
 
@@ -100,7 +99,6 @@
         //prima tekst
         let text=opis.value;
         //brise iz html-a
-        // opis.innerHTML="";
         opis.value="";
         //izbacuje visak iz teksta i pravi niz od ostatka
         text=text.split(cut);
