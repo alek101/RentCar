6c5b96a19610d800a1ec68318ffcd3160a8e78df
alek101 <47416711+alek101@users.noreply.github.com>
2019-11-29 11:16:42 +0100

    Verzija 4.6.2
    Ads su sortirane po ceni.

--- app/Http/Controllers/AdsController.php
+++ app/Http/Controllers/AdsController.php
@@ -16,10 +16,13 @@ class AdsController extends Controller
     public function makeJSONforAds()
     {
         $json=[];
+        //pomocni niz kako bismo sortirali json po cenama
+        $cene=[];
         $aktiveModels=TipoviAutomobilaResource::modeliAktivni();
         
         foreach($aktiveModels as $model)
         {
+            //napravimo json sa podacima koji nam trebaju
             $pom=[];
             $pom['Model']=$model->Model;
             $pom['slika']=$model->slika;
@@ -28,12 +31,18 @@ class AdsController extends Controller
             $pom['Broj_torbi']=$model->Broj_torbi;
             $pom['Broj_sedista']=$model->Broj_sedista;
 
+            //racunamo cenu za 7 dana
             $dateStart=date('Y-m-d');
             $dateEnd=date('Y-m-d', strtotime($dateStart." + 7 days"));
-            $pom['cena']=PomFunkResource::totalCost($model->Model,$dateStart,$dateEnd);
+            $cena=PomFunkResource::totalCost($model->Model,$dateStart,$dateEnd);
+            $pom['cena']=$cena;
+
+            array_push($cene,$cena);
 
             array_push($json,$pom);
         }
+
+        array_multisort($cene,$json);
 
         return json_encode($json);
     }
