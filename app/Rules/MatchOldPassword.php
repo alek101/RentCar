10590c43f42c6b6dfce4ad046a34db99ee14f6ca
alek101 <47416711+alek101@users.noreply.github.com>
2019-12-02 00:26:54 +0100

    Verzija 4.9.2
    for record

--- app/Rules/MatchOldPassword.php
+++ app/Rules/MatchOldPassword.php
@@ -3,6 +3,7 @@
 namespace App\Rules;
 
 use Illuminate\Contracts\Validation\Rule;
+use Illuminate\Support\Facades\Hash;
 
 class MatchOldPassword implements Rule
 {
@@ -25,7 +26,7 @@ class MatchOldPassword implements Rule
      */
     public function passes($attribute, $value)
     {
-        //
+        return Hash::check($value, auth()->user()->password);
     }
 
     /**
@@ -35,6 +36,6 @@ class MatchOldPassword implements Rule
      */
     public function message()
     {
-        return 'The validation error message.';
+        return ':attribute je pogrešna.';
     }
 }
